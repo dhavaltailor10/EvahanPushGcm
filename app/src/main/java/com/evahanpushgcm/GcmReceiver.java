cdffe70f9f67e7a113a7a5ececbd33d588ceee48
Dhaval Tailor <dhavaltailor10593@gmail.com>
2016-08-24 12:10:42 +0530

    Update GcmReceiver.java

--- app/src/main/java/com/evahanpushgcm/GcmReceiver.java
+++ app/src/main/java/com/evahanpushgcm/GcmReceiver.java
@@ -6,9 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
-/**
- * Created by Dreamworld Solutions on 22-7-2016.
- */
 public class GcmReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
